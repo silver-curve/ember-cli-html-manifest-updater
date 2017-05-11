c01871c053c1530651f5c813edddf0a9fb5ea4a5
Flavio Signorelli <flavio.signorelli@silvercurve.co.uk>
2017-05-11 14:52:01 +0100

    Fixed timing and added console logs to help debug

--- addon/appcache/service.js
+++ addon/appcache/service.js
@@ -2,28 +2,38 @@ import Ember from 'ember';
 import { task, timeout } from 'ember-concurrency';
 import config from 'ember-get-config';
 
+const { get } = Ember;
+
 export default Ember.Service.extend({
 
 	init() {
 		this._super(...arguments);
 		console.log("htmlManifestUpdaterService: initializing...");
 		const applicationCache = this.get("appCache");
+		console.log("htmlManifestUpdaterService: enabled: "+ get(this, "timerUpdateEnabled"));
+		console.log("htmlManifestUpdaterService: hour: "+ get(this, "timerHour"));
 
 		// registering the AppCache UpdateReady handler
 		applicationCache.addEventListener('updateready', this._updateReady, false);
 
 		// if enabled, start waiting for the daily check time
-		if (this.get("timerUpdateEnabled")) {
+		if (get(this, "timerUpdateEnabled")) {
 			this.get("updateLaterTask").perform();
+			console.log(`htmlManifestUpdaterService: checking for updates at ${get(this, "timerHour")}:00`);
+		}
+		else
+		{
+			console.log("htmlManifestUpdaterService: checking only at page load");
 		}
 
-		console.log("htmlManifestUpdaterService: started!");
+		this._updateReady();
 	},
 
 	willDestroy() {
 		const applicationCache = this.get("appCache");
 		// removes the event listener from the AppCache
 		applicationCache.removeEventListener('updateready', this._updateReady, false);
+		console.log("htmlManifestUpdaterService: service removed");
 		this._super(...arguments);
 	},
 
@@ -38,20 +48,26 @@ export default Ember.Service.extend({
 			try {
 				let delay = 0;
 				const date = new Date();
-				const timeToCheck = (new Date()).setHours(this.timerHour);
-				const offset = Math.abs(timeToCheck - date);
+				console.log("htmlManifestUpdaterService: date = " + date);
+				const timeToCheck = (new Date()).setHours(get(this, "timerHour"));
+				console.log("htmlManifestUpdaterService: timeToCheck = " + timeToCheck);
+				const offset = timeToCheck - date;
+				console.log("htmlManifestUpdaterService: offset = " + offset);
 				
 				// should we still check today?
 				if(offset > 0) {
 					// schedule it for later today
 					delay = offset;
+					console.log(`htmlManifestUpdaterService: checking today in ${delay}ms`);
 				}
 				else {
 					// schedule it for tomorrow
 					let tomorrow = new Date();
 					tomorrow.setDate(date.getDate() + 1);
-					tomorrow.setHours(this.timerHour);
-					delay = Math.abs(tomorrow - date);
+					tomorrow.setHours(get(this, "timerHour"));
+					console.log("htmlManifestUpdaterService: tomorrow = " + tomorrow);
+					delay = tomorrow - date;
+					console.log(`htmlManifestUpdaterService: checking tomorrow (in ${delay}ms)`);
 				}
 
 				yield timeout(delay);
@@ -60,7 +76,7 @@ export default Ember.Service.extend({
 				applicationCache.update();
 			}
 			catch(err) {
-				console.log("HTML Manifest update error:\n"+err);
+				console.log("htmlManifestUpdaterService: update error:\n"+err);
 			}
 		}
 	}).drop(),
@@ -68,6 +84,7 @@ export default Ember.Service.extend({
 	// Event handler -- When the update is ready, reload the page
 	_updateReady() {
 		const applicationCache = this.get("appCache");
+		console.log("htmlManifestUpdaterService: _updateReady called");
 		if (applicationCache.status === applicationCache.UPDATEREADY) {
 			// use the new manifest
 			applicationCache.swapCache();
